623592c6e02f218cd2b364521538e61213df1a6c
Sergei Popinevskii <gurza000@gmail.com>
2024-09-21 09:57:46 +0300

    implement upload-pack

--- daemon.go
+++ daemon.go
@@ -3,9 +3,11 @@ package daemon
 import (
 	"context"
 	"errors"
+	"fmt"
 	"io"
 
 	"github.com/go-git/go-billy/v5"
+	"github.com/go-git/go-git/v5/plumbing/protocol/packp"
 	"github.com/go-git/go-git/v5/plumbing/transport"
 	"github.com/go-git/go-git/v5/plumbing/transport/server"
 )
@@ -29,4 +31,35 @@ func InfoRefs(ctx context.Context, fs billy.Filesystem, repoPath string, w io.Wr
 		return errors.New("failed to advertise references")
 	}
 	return refs.Encode(w)
+}
+
+func UploadPack(ctx context.Context, fs billy.Filesystem, repoPath string, r io.Reader, w io.Writer) error {
+	srv := server.NewServer(server.NewFilesystemLoader(fs))
+
+	ep, err := transport.NewEndpoint(repoPath)
+	if err != nil {
+		return fmt.Errorf("failed to create endpoint: %w", err)
+	}
+
+	sess, err := srv.NewUploadPackSession(ep, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create session: %w", err)
+	}
+	defer sess.Close()
+
+	req := packp.NewUploadPackRequest()
+	if err := req.Decode(r); err != nil {
+		return fmt.Errorf("failed to decode request: %w", err)
+	}
+
+	resp, err := sess.UploadPack(ctx, req)
+	if err != nil {
+		return fmt.Errorf("failed to process upload-pack request: %w", err)
+	}
+
+	if err := resp.Encode(w); err != nil {
+		return fmt.Errorf("failed to encode response: %w", err)
+	}
+
+	return nil
 }
