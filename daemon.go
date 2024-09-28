d734135f936193e65afc4402c524fac962e509c6
Sergei Popinevskii <gurza000@gmail.com>
2024-09-28 23:14:17 +0300

    refactor: rename

--- daemon.go
+++ daemon.go
@@ -13,11 +13,11 @@ import (
 	"github.com/go-git/go-git/v5/plumbing/transport/server"
 )
 
-type ServiceType string
+type GitServiceType string
 
 const (
-	ServiceUploadPack  ServiceType = "git-upload-pack"
-	ServiceReceivePack ServiceType = "git-receive-pack"
+	GitServiceUploadPack  GitServiceType = "git-upload-pack"
+	GitServiceReceivePack GitServiceType = "git-receive-pack"
 )
 
 type Service struct {
@@ -25,7 +25,7 @@ type Service struct {
 	ep  *transport.Endpoint
 }
 
-func New(fs billy.Filesystem, repo string) (*Service, error) {
+func NewService(fs billy.Filesystem, repo string) (*Service, error) {
 	srv := server.NewServer(server.NewFilesystemLoader(fs))
 	ep, err := transport.NewEndpoint(repo)
 	if err != nil {
@@ -34,34 +34,34 @@ func New(fs billy.Filesystem, repo string) (*Service, error) {
 	return &Service{srv: srv, ep: ep}, nil
 }
 
-func (s *Service) newSession(svc ServiceType) (transport.Session, error) {
+func (s *Service) newSession(typ GitServiceType) (transport.Session, error) {
 	var (
 		sess transport.Session
 		err  error
 	)
 
-	switch svc {
-	case ServiceUploadPack:
+	switch typ {
+	case GitServiceUploadPack:
 		sess, err = s.srv.NewUploadPackSession(s.ep, nil)
-	case ServiceReceivePack:
+	case GitServiceReceivePack:
 		sess, err = s.srv.NewReceivePackSession(s.ep, nil)
 	default:
-		return nil, fmt.Errorf("unsupported service type: %s", svc)
+		return nil, fmt.Errorf("unsupported service type: %s", typ)
 	}
 
 	if err != nil {
 		if errors.Is(err, transport.ErrRepositoryNotFound) {
 			return nil, fmt.Errorf("repository not found: %q", s.ep.Path)
 		}
-		return nil, fmt.Errorf("failed to create %s session for %q: %w", svc, s.ep.Path, err)
+		return nil, fmt.Errorf("failed to create %s session for %q: %w", typ, s.ep.Path, err)
 	}
 
 	return sess, nil
 }
 
 // InfoRefs retrieves the advertised references for the given repository.
-func (s *Service) InfoRefs(ctx context.Context, svc ServiceType) (*packp.AdvRefs, error) {
-	sess, err := s.newSession(svc)
+func (s *Service) InfoRefs(ctx context.Context, typ GitServiceType) (*packp.AdvRefs, error) {
+	sess, err := s.newSession(typ)
 	if err != nil {
 		return nil, err
 	}
@@ -72,7 +72,7 @@ func (s *Service) InfoRefs(ctx context.Context, svc ServiceType) (*packp.AdvRefs
 		return nil, fmt.Errorf("failed to retrieve advertised references: %w", err)
 	}
 	res.Prefix = [][]byte{
-		[]byte(fmt.Sprintf("# service=%s", svc)),
+		[]byte(fmt.Sprintf("# service=%s", typ)),
 		pktline.Flush,
 	}
 	// FIXME: add no-thin capability to work-around some go-git limitations
@@ -90,7 +90,7 @@ func (s *Service) UploadPack(ctx context.Context, r io.Reader) (*packp.UploadPac
 		return nil, fmt.Errorf("failed to decode upload-pack request: %w", err)
 	}
 
-	sess, err := s.newSession(ServiceUploadPack)
+	sess, err := s.newSession(GitServiceUploadPack)
 	if err != nil {
 		return nil, err
 	}
@@ -117,7 +117,7 @@ func (s *Service) ReceivePack(ctx context.Context, r io.Reader) (*packp.ReportSt
 		return nil, fmt.Errorf("failed to decode receive-pack request: %w", err)
 	}
 
-	sess, err := s.newSession(ServiceReceivePack)
+	sess, err := s.newSession(GitServiceReceivePack)
 	if err != nil {
 		return nil, err
 	}
